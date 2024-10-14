686739aa7f561491fe3e83587ab9389b067c7685
goodsun <goodsun0317@gmail.com>
2024-10-14 21:19:10 +0900

    DMにも転送

--- src/controller/getkeyController.ts
+++ src/controller/getkeyController.ts
@@ -7,6 +7,7 @@ import creatorService from "../service/creatorService.js";
 const connect = async (req) => {
   const message = await creatorService.getKey(req);
   await discordService.interResponse(message, req);
+  await discordService.sendDiscordDm(message, req.member.user.id);
 
   //let sendMes = `getkey ${req.data.options[0].value} を受け付けました\nセキュリティのためDMにてsecretをお送りします。`;
   //await discordService.sendDiscordResponse(sendMes, req.token, "notResend");
